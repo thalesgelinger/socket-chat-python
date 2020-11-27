fc143796eb8417f7239e556093cb85c06b5df4b7
thalesgelinger <thalesgelinger@gmail.com>
2020-11-27 19:57:38 -0300

    refact: remove comments

--- server.py
+++ server.py
@@ -12,22 +12,12 @@ def get_port():
         exit()
 
 def set_socket(port):
-    """The first argument AF_INET is the address domain of the 
-    socket. This is used when we have an Internet Domain with 
-    any two hosts The second argument is the type of socket. 
-    SOCK_STREAM means that data or characters are read in 
-    a continuous flow."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
     
     HOST_NAME = socket.gethostname()
     IP_ADDRESS = socket.gethostbyname(HOST_NAME)
     
-    """ 
-    binds the server to an entered IP address and at the 
-    specified port number. 
-    The client must be aware of these parameters 
-    """
     server.bind((IP_ADDRESS, port)) 
     server.listen(100) 
 
@@ -70,19 +60,9 @@ def remove(connection):
         list_of_clients.remove(connection) 
   
 while True: 
-  
-    """Accepts a connection request and stores two parameters,  
-    conn which is a socket object for that user, and addr  
-    which contains the IP address of the client that just  
-    connected"""
     conn, addr = server.accept() 
-  
-    """Maintains a list of clients for ease of broadcasting 
-    a message to all available people in the chatroom"""
     list_of_clients.append(conn) 
-
     print addr[0] + " se conectou"
-  
     start_new_thread(clientthread,(conn,addr))     
   
 conn.close() 
